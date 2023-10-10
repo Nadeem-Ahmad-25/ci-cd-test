501dd1a0d9c8412d513f4770191d6f6d86aa1616
Nadeem-Ahmad-25 <139974080+Nadeem-Ahmad-25@users.noreply.github.com>
2023-10-10 16:54:58 +0530

    Update script-local.sh

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-test-instance.pem" ubuntu@ec2-13-233-246-213.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-test-instance.pem" ubuntu@ec2-13-232-26-160.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
